175035beae681c0d9e67c46b694da721a4d420c5
TDavydov <timurl1kec0de@gmail.com>
2024-07-27 13:02:24 +0500

    Add project files.

---
+++ EazyCSharp/DynamicProgrammingExampleProblem.cs
@@ -0,0 +1,104 @@
+﻿namespace EazyCSharp
+{
+    internal class DynamicProgrammingExampleProblem
+    {
+        private readonly int[,] a;
+        private readonly int[,] b;
+        private readonly int[,,] paths;
+        private readonly int[,] aHighlight;
+        private readonly int rows;
+        private readonly int columns;
+
+        public DynamicProgrammingExampleProblem()
+        {
+            a = new int[,] {
+                { 9, 8, 6, 2 },
+                { 10, 111, 12, 11 },
+                { 3, 7, 12, 8 },
+                { 5, 9, 12, 90 }
+            };
+            aHighlight = new int[4, 4];
+            b = new int[4, 4];
+            paths = new int[4, 4, 2];
+
+            rows = a.GetUpperBound(0) + 1;
+            columns = a.Length / rows;
+        }
+
+        public void Run()
+        {
+            Recursive(rows - 1, 0, rows - 1, 0);
+
+            PrintMatrix(a);
+            PrintMatrix(b);
+
+            CalcPath();
+
+            PrintMatrix(a, true);
+        }
+
+        void Recursive(int i, int j, int fromI, int fromJ)
+        {
+            if (i < 0 || j == columns)
+                return;
+
+            var newValue = a[i, j] + b[fromI, fromJ];
+            if (newValue > b[i, j])
+            {
+                b[i, j] = newValue;
+                paths[i, j, 0] = fromI;
+                paths[i, j, 1] = fromJ;
+            }
+
+            Recursive(i - 1, j, i, j);
+            Recursive(i, j + 1, i, j);
+        }
+
+        void CalcPath()
+        {
+            var i = 0;
+            var j = columns - 1;
+            var tmpI = i;
+            var tmpJ = j;
+            do
+            {
+                aHighlight[i, j] = 1;
+                tmpI = paths[i, j, 0];
+                tmpJ = paths[i, j, 1];
+                i = tmpI;
+                j = tmpJ;
+            }
+            while (!(i == 3 && j == 0));
+            aHighlight[3, 0] = 1;
+        }
+
+        void PrintMatrix(int[,] matrix, bool isHighlight = false)
+        {
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < columns; j++)
+                {
+                    if (isHighlight && aHighlight[i, j] == 1)
+                        Console.Write($"*{matrix[i, j]}*\t");
+                    else
+                        Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        void PrintPaths()
+        {
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < columns; j++)
+                {
+                    Console.Write($"({paths[i, j, 0]}, {paths[i, j, 1]})\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ EazyCSharp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+
+var a = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+var n = a.Length;
+var k = 3;
+
+var indexes = Enumerable.Range(0, k).ToArray();
+
+var exit = false;
+var iterationCount = 0;
+while (!exit)
+{
+    iterationCount++;
+    PrintCombination();
+
+    var nextIndex = indexes[indexes.Length - 1] + 1;
+
+    if (nextIndex == n)
+    {
+        var cursor = indexes.Length - 2;
+        var count = indexes.Length - cursor - 1;
+        while (indexes[cursor] + 1 + count >= n)
+        {
+            cursor--;
+            count++;
+
+            if (cursor < 0)
+            {
+                exit = true;
+                break;
+            }
+        }
+
+        if (!exit)
+        {
+            indexes[cursor]++;
+            cursor++;
+            while (cursor < indexes.Length)
+            {
+                indexes[cursor] = indexes[cursor - 1] + 1;
+                cursor++;
+            }
+        }
+    } 
+    else
+    {
+        indexes[indexes.Length - 1] = nextIndex;
+    }
+}
+Console.WriteLine($"Кол-во итераций: {iterationCount}");
+
+void PrintCombination()
+{
+    sb.Clear();
+    for (var i = 0; i < k; i++)
+    {
+        if (i < k - 1)
+            sb.Append($"{a[indexes[i]]}, ");
+        else
+            sb.Append($"{a[indexes[i]]}");
+    }
+    Console.WriteLine(sb.ToString());
+}
